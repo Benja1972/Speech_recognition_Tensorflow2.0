360af72c4e493e097415e350e975af2956db1c8c
Benja1972 <benja@bk.ru>
2019-12-11 15:10:05 +0100

    new files

---
+++ README.md
@@ -0,0 +1,28 @@
+Spoken words recognition with TensorFlow
+========================================
+
+## Speech Commands Data Set v0.02
+
+This is a set of one-second .wav audio files, each containing a single spoken
+English word. These words are from a small set of commands, and are spoken by a
+variety of different speakers. The audio files are organized into folders based
+on the word they contain, and this data set is designed to help train simple
+machine learning models. This dataset is covered in more detail at [https://arxiv.org/abs/1804.03209](https://arxiv.org/abs/1804.03209).
+
+It's licensed under the [Creative Commons BY 4.0
+license](https://creativecommons.org/licenses/by/4.0/). See the LICENSE
+file in this folder for full details. Its original location was at
+[http://download.tensorflow.org/data/speech_commands_v0.02.tar.gz](http://download.tensorflow.org/data/speech_commands_v0.02.tar.gz).
+
+
+## Sound samples
+Sound samples is processed to MFCCs and used as input matrix of shape (16,44) 
+
+
+## Model
+
+Model is composed of two Dense layers with Dropouts for regularization 
+
+## Validation 
+![validation graph](im/validation.png)
+
---
+++ python/speech_recognition.py
@@ -0,0 +1,136 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from random import sample 
+
+import librosa
+import librosa.display
+from os.path import join 
+import glob
+import os 
+
+
+SAMPLING_RATE=16000
+DUR_WAV = 22050
+
+NUM_MFCC = 12
+
+words = ['one', 'two'] #, 'three', 'four', 'five'
+
+
+df  = pd.DataFrame()
+
+for wr in words:
+
+    flph = join("../data",wr)
+
+    wf = [f for f in glob.glob(join(flph,"*.wav"))]
+    wf = sample(wf,800) ####### REMOVE ###############
+
+    w_df = pd.DataFrame({'file':wf, 'labels':[wr]*len(wf)})
+    
+    df = pd.concat([df, w_df])
+    
+    df = df.sample(frac=1)
+
+
+def mfccf(raw, n_mfccs=16):
+    wave, sample_rate = librosa.load(raw['file'], sr = SAMPLING_RATE, res_type='kaiser_fast')
+    mfccs = librosa.feature.mfcc(y=wave, sr=SAMPLING_RATE, n_mfcc=n_mfccs)
+    mfccf = np.mean(mfccs.T,axis=0)
+    return mfccf
+
+def mfccs(raw, n_mfccs=16):
+    wave, sample_rate = librosa.load(raw['file'], sr = SAMPLING_RATE, res_type='kaiser_fast')
+    if len(wave) != DUR_WAV:
+        wave = librosa.effects.time_stretch(wave, len(wave)/DUR_WAV)
+    mfccs = librosa.feature.mfcc(y=wave, sr=SAMPLING_RATE, n_mfcc=n_mfccs)
+
+    return mfccs
+    
+
+Feature = lambda raw: mfccs(raw, n_mfccs=NUM_MFCC)
+
+df['feats'] = df.apply(Feature, axis=1)
+
+import tensorflow as tf
+from sklearn.preprocessing import LabelEncoder
+
+
+X = np.array(df['feats'].tolist())
+y = np.array(df['labels'].tolist())
+
+
+## Normalization (<--??? https://www.kaggle.com/c/freesound-audio-tagging/discussion/54082)
+#
+#~ mean = np.mean(X, axis=2)
+#~ std = np.std(X, axis=2)
+#~ for i in range(X.shape[2]): 
+    #~ X[:,:,i] = (X[:,:,i]-mean)/std 
+
+
+mean = np.mean(X)
+std = np.std(X)
+X = (X - mean)/std
+
+lb = LabelEncoder()
+
+y = tf.keras.utils.to_categorical(lb.fit_transform(y))
+
+
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+if True:
+    from tensorflow.keras.models import Sequential
+    from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
+
+
+    num_labels = y.shape[1]
+
+    # build model
+    model = Sequential()
+    model.add(Flatten(input_shape=(NUM_MFCC, 44)))
+    model.add(Dense(256, activation="relu"))
+    model.add(Dropout(0.5))
+
+    model.add(Dense(256, activation="relu"))
+    model.add(Dropout(0.5))
+
+    model.add(Dense(num_labels, activation="softmax"))
+
+    model.compile(loss='categorical_crossentropy', metrics=['accuracy'], optimizer='adam')
+
+    history = model.fit(X_train, y_train, 
+                        batch_size=32, 
+                        epochs=30, 
+                        validation_data=(X_test, y_test))
+    
+    ## ==  Verify
+    # Plot training & validation accuracy values
+    
+    fig, (ax1, ax2) = plt.subplots(2, sharex=True)
+    fig.suptitle('Validation and accuracy values')
+
+    
+    ax1.plot(history.history['accuracy'], 'o-', label='Training accuracy')
+    ax1.plot(history.history['val_accuracy'], 'o-', label='Validation accuracy')
+    ax1.set_title('Model accuracy')
+    ax1.set_ylabel('Accuracy')
+    ax1.legend(['Train', 'Val'], loc='upper left')
+
+
+    # Plot training & validation loss values
+    ax2.plot(history.history['loss'], 'o-', label='Training loss')
+    ax2.plot(history.history['val_loss'], 'o-', label='Validation loss')
+    ax2.set_title('Model loss')
+    ax2.set_ylabel('Loss')
+    ax2.set_xlabel('Epoch')
+    ax2.legend(['Train', 'Val'], loc='upper left')
+    plt.show()
+
+
+
+
